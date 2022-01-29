6ac33bc296f351fc40a9f09274cee0780ae98ab5
Thomas D <duongthomas7@gmail.com>
2022-01-28 19:30:28 -0600

    added button to change number of squares per side

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 const container = document.querySelector('.container');
 
 for (i=0; i<16; i++){
-    const row = document.createElement('div');
-    row.classList.toggle("row");
+    const column = document.createElement('div');
+    column.classList.toggle("column");
     for (j = 0; j < 16; j++){
-        const color = document.createElement('div');
-        color.classList.toggle("paint");
-        row.appendChild(color);
+        const square = document.createElement('div');
+        square.classList.toggle("paint");
+
+        square.addEventListener("mouseover", () => {
+            square.style["background-color"] = "blue";
+            square.style.height = 960/16;
+            square.style.weight = 960/16;
+        });
+
+        column.appendChild(square);
         
     }
-    console.log('row');
-    container.appendChild(row);
+    container.appendChild(column);
 }
 
+const tile = document.querySelectorAll(".paint");
+
+const reset = document.querySelector(".reset");
+
+
+reset.addEventListener("click", () => {
+    const gridColumn = document.querySelectorAll(".column");
+    gridColumn.forEach(column => {
+        container.removeChild(column);
+    })
+
+    let userGrid = prompt("Enter the # of squares per side: ");
+    for (i=0; i<userGrid; i++){
+        const column = document.createElement('div');
+        column.classList.toggle("column");
+        for (j = 0; j < 16; j++){
+            const square = document.createElement('div');
+            square.classList.toggle("paint");
+
+            square.addEventListener("mouseover", () => {
+                square.style["background-color"] = "blue";
+                square.style.height = 960/16;
+                square.style.weight = 960/16;
+            });
+
+            column.appendChild(square);
+            
+        }
+        container.appendChild(column);
+    }
+})
