84407a26fe1eba765bfc1fb09ffcdee55d01c64a
Thomas D <duongthomas7@gmail.com>
2022-01-28 18:11:48 -0600

    added 16x16 divs

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.querySelector('.container');
 
 for (i=0; i<16; i++){
-    const div = document.createElement('div');
-    div.classList.toggle("colored");
-    container.appendChild(div);
+    const row = document.createElement('div');
+    row.classList.toggle("row");
+    for (j = 0; j < 16; j++){
+        const color = document.createElement('div');
+        color.classList.toggle("paint");
+        row.appendChild(color);
+    }
+    console.log('row');
+    container.appendChild(row);
 }
